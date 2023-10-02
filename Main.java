66bcbb896ff89e38c5f446500e426432019de189
Insignate <46281639+Insignate@users.noreply.github.com>
2023-10-02 13:15:35 -0400

    Project 2

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Input input = new Input();
+    public static void main(String[] args)  {
+        //Input input = new Input();
+
+        String filePath = "";
+
+        int smokerQtt = 0;
+        int nonSmokerQtt = 0;
 
         final String SMOKER = "smoker";
         final String NON_SMOKER = "non-smoker";
@@ -12,19 +21,48 @@ public class Main {
         final double WEIGHT_LOWER_BOUND = 0;
         final double WEIGHT_HIGHER_BOUND = 2000;
 
+//        Policy policy = new Policy(
+//            (int)input.scanNumber("Please enter the Policy Number: "),
+//            input.scanString("Please enter the Provider Name: "),
+//            input.scanString("Please enter the Policyholder’s First Name: "),
+//            input.scanString("Please enter the Policyholder’s Last Name: "),
+//            (int)input.scanNumberBetween("Please enter the Policyholder’s Age: ", AGE_LOWER_BOUND, AGE_HIGH_BOUND),
+//            input.scanBoolean(String.format("Please enter the Policyholder’s Smoking Status ("+ SMOKER +"/"+ NON_SMOKER +"): "),SMOKER, NON_SMOKER),
+//            input.scanNumberBetween("Please enter the Policyholder’s Height (in inches): ", HEIGHT_LOWER_BOUND, HEIGHT_HIGH_BOUND),
+//            input.scanNumberBetween("Please enter the Policyholder’s Weight (in pounds): ", WEIGHT_LOWER_BOUND, WEIGHT_HIGHER_BOUND)
+//        );
+//        System.out.println(policy);
 
+        try{
+            FileManager fileManager = new FileManager();
+            filePath = fileManager.getFileLocation();
+            System.out.println(filePath);
+            File file = new File(filePath);
+            Scanner scanFile = new Scanner(file);
+            while(scanFile.hasNextLine()){
+                Policy policy = new Policy(
+                        Integer.parseInt(scanFile.nextLine()),
+                        scanFile.nextLine(),
+                        scanFile.nextLine(),
+                        scanFile.nextLine(),
+                        Integer.parseInt(scanFile.nextLine()),
+                        (Objects.equals(scanFile.nextLine(), SMOKER)),
+                        Double.parseDouble(scanFile.nextLine()),
+                        Double.parseDouble(scanFile.nextLine())
+                );
+                if (scanFile.hasNextLine()) scanFile.nextLine();
+                System.out.println(policy);
+                if(policy.isSmoker()) smokerQtt++;
+                else nonSmokerQtt++;
+                System.out.println();
+            }
 
-        Policy policy = new Policy(
-            (int)input.scanNumber("Please enter the Policy Number: "),
-            input.scanString("Please enter the Provider Name: "),
-            input.scanString("Please enter the Policyholder’s First Name: "),
-            input.scanString("Please enter the Policyholder’s Last Name: "),
-            (int)input.scanNumberBetween("Please enter the Policyholder’s Age: ", AGE_LOWER_BOUND, AGE_HIGH_BOUND),
-            input.scanBoolean(String.format("Please enter the Policyholder’s Smoking Status ("+ SMOKER +"/"+ NON_SMOKER +"): "),SMOKER, NON_SMOKER),
-            input.scanNumberBetween("Please enter the Policyholder’s Height (in inches): ", HEIGHT_LOWER_BOUND, HEIGHT_HIGH_BOUND),
-            input.scanNumberBetween("Please enter the Policyholder’s Weight (in pounds): ", WEIGHT_LOWER_BOUND, WEIGHT_HIGHER_BOUND)
-        );
-        System.out.println(policy);
+            System.out.println("Smokers: " + smokerQtt);
+            System.out.println("Non-Smokers: " + nonSmokerQtt);
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
 
     }
 }
